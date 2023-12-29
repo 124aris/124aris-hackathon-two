55fe17446e65e84fdd5af4580b4cdcfe97a7d6c3
Muhammad Haris <mharis01470017@gmail.com>
2023-12-29 16:28:06 +0500

    hackathon-two

--- src/views/Homepage/Secondsection.tsx
+++ src/views/Homepage/Secondsection.tsx
@@ -8,7 +8,7 @@ export default  function Secondsection() {
                 <span className="text-xs text-blue-600 font-bold">PROMOTIONS</span>
                 <h2 className="scroll-m-20 pb-2 text-4xl font-bold tracking-tight first:mt-0">Our Promotions Events</h2>
             </div>
-            <div className="flex justify-center space-x-8 items-stretch py-8">
+            <div className="flex justify-center space-x-8 items-stretch py-7">
                 <div className="flex flex-col basis-2/5 space-y-4">
                     <div className="flex bg-[#d6d6d8] justify-between items-center">
                         <div className="flex flex-col items-center justify-center ml-8">
@@ -27,17 +27,27 @@ export default  function Secondsection() {
                     <div className="flex flex-col bg-[#efe1c7] justify-end">
                         <div className="ml-5 mb-4">
                             <p className="leading-7 [&:not(:first-child)]:mt-6">Flex Sweatshirt</p>
-                            <span className="font-normal text-base">$100.00</span>
-                            <span className="text-lg font-semibold ml-2">$75.00</span>
+                            <div className="flex items-center">
+                                <div className="relative">
+                                    <p className="font-normal text-base">$100.00</p>
+                                    <div className="absolute bottom-3 h-full w-full border-b border-black"></div>
+                                </div>
+                                <p className="text-lg font-semibold ml-2">$75.00</p>
+                            </div>
                         </div>
                         <Image src={"/event2.webp"} alt="" width={282} height={368}/>
                     </div>
                     <div className="flex flex-col bg-[#d7d7d9] justify-end">
                         <div className="ml-5 mb-2">
                             <p className="leading-7 [&:not(:first-child)]:mt-6">Flex Push Button Bomber</p>
-                            <span className="font-normal text-base">$225.00</span>
-                            <span className="text-lg font-semibold ml-2">$190.00</span>
+                            <div className="flex items-center">
+                                <div className="relative">
+                                    <p className="font-normal text-base">$225.00</p>
+                                    <div className="absolute bottom-3 h-full w-full border-b border-black"></div>
+                                </div>
+                                <p className="text-lg font-semibold ml-2">$190.00</p>
                             </div>
+                        </div>
                         <Image src={"/event3.webp"} alt="" width={282} height={368}/>
                     </div>
                 </div>
