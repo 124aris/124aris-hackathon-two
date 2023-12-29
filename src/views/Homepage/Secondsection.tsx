2eb88f7301fabbfde6fd0be4fe135ad8d6e3eb4d
Muhammad Haris <mharis01470017@gmail.com>
2023-12-29 13:08:01 +0500

    hackathon-two

--- src/views/Homepage/Secondsection.tsx
+++ src/views/Homepage/Secondsection.tsx
@@ -8,22 +8,32 @@ export default  function Secondsection() {
                 <span className="text-xs text-blue-600 font-bold">PROMOTIONS</span>
                 <h2 className="scroll-m-20 pb-2 text-4xl font-bold tracking-tight first:mt-0">Our Promotions Events</h2>
             </div>
-            <div className="grid grid-cols-2">
-                <div className="flex flex-col">
-                    <div>
+            <div className="flex justify-around items-center py-8">
+                <div className="flex flex-col space-y-4">
+                    <div className="flex bg-[#d6d6d8] justify-center items-center">
+                        <div className="flex flex-col items-center justify-center">
+                            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">GET UP TO <span>60%</span></h3>
+                            <p className="leading-7 [&:not(:first-child)]:mt-6">For the summer season</p>
+                        </div>
                         <Image src={"/event1.webp"} alt="" width={282} height={218}/>
                     </div>
-                    <div>
+                    <div className="flex flex-col justify-center items-center bg-[#212121] text-white py-10 px-12">
                         <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">GET 30% Off</h3>
                         <p className="leading-7 [&:not(:first-child)]:mt-6">USE PROMO CODE</p>
-                        <Button>DINEWEEKENDSALE</Button>
+                        <Button variant="ghost" className="bg-[#474747] text-white hover:bg-[#474747] hover:text-white">DINE WEEKEND SALE</Button>
                     </div>
                 </div>
-                <div className="flex">
-                    <div>
+                <div className="flex space-x-4">
+                    <div className="bg-[#efe1c7]">
+                        <p className="leading-7 [&:not(:first-child)]:mt-6">Flex Sweatshirt</p>
+                        <span>$100.00</span>
+                        <span>$75.00</span>
                         <Image src={"/event2.webp"} alt="" width={282} height={362}/>
                     </div>
-                    <div>
+                    <div className="bg-[#d7d7d9]">
+                        <p className="leading-7 [&:not(:first-child)]:mt-6">Flex Push Button Bomber</p>
+                        <span>$225.00</span>
+                        <span>$190.00</span>
                         <Image src={"/event3.webp"} alt="" width={282} height={368}/>
                     </div>
                 </div>
