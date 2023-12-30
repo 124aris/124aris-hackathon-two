03fce418b5cf534a675bbfe8c0269abdbc4773e8
Muhammad Haris <mharis01470017@gmail.com>
2023-12-30 22:11:48 +0500

    hackathon-two

---
+++ src/items/itemslist.tsx
@@ -0,0 +1,169 @@
+const ItemsList = [
+    {
+        ID: 1,
+        ProductName: "Brushed Raglan Sweatshirt",
+        ProductType: "Sweater",
+        Price: 195.00,
+        ProductCategory: "Female",
+        ProductInformation: {
+            ProductDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ProductCare1: "Hand wash using cold water.",
+            ProductCare2: "Do not using bleach.",
+            ProductCare3: "Hang it to dry.",
+            ProductCare4: "Iron on low temperature."
+        },
+        ProductPicture1:"/Item1.png",
+    },
+    {
+        ID: 2,
+        ProductName: "Cameryn Sash Tie Dress",
+        ProductType: "Dress",
+        Price: 545.00,
+        ProductCategory: "Female",
+        ProductInformation: {
+            ProductDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ProductCare1: "Lorem ipsum dolor sit amet",
+            ProductCare2: "Lorem ipsum dolor sit amet",
+        },
+        ProductPicture1:"/Item2.1.png",
+        ProductPicture2:"/Item2.2.png",
+        ProductPicture3:"/Item2.3.png",
+        ProductPicture4:"/Item2.4.png",
+    },
+    {
+        ID: 3,
+        ProductName: "Flex Sweatshirt",
+        ProductType: "Sweater",
+        Price: 175.00,
+        ProductCategory: "Female",
+        ProductInformation: {
+            ProductDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ProductCare1: "Hand wash using cold water.",
+            ProductCare2: "Do not using bleach.",
+            ProductCare3: "Hang it to dry.",
+        },
+        ProductPicture1:"/Item3.png",
+    },
+    {
+        ID: 4,
+        ProductName: "Flex Sweatpants",
+        ProductType: "Pants",
+        Price: 175.00,
+        ProductCategory: "Female",
+        ProductInformation: {
+            ProductDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ProductCare1: "Hand wash using cold water.",
+            ProductCare2: "Do not using bleach.",
+            ProductCare3: "Hang it to dry.",
+            ProductCare4: "Iron on low temperature."
+        },
+        ProductPicture1:"/Item4.png",
+    },
+    {
+        ID: 5,
+        ProductName: "Pink Fleece Sweatpants",
+        ProductType: "Pants",
+        Price: 195.00,
+        ProductCategory: "Female",
+        ProductInformation: {
+            ProductDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ProductCare1: "Hand wash using cold water.",
+            ProductCare2: "Do not using bleach.",
+            ProductCare3: "Hang it to dry.",
+            ProductCare4: "Iron on low temperature."
+        },
+        ProductPicture1:"/Item5.png",
+    },
+    {
+        ID: 6,
+        ProductName: "Lite Sweatpants",
+        ProductType: "Pants",
+        Price: 150.00,
+        ProductCategory: "Female",
+        ProductInformation: {
+            ProductDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ProductCare1: "Hand wash using cold water.",
+            ProductCare2: "Do not using bleach.",
+            ProductCare3: "Hang it to dry.",
+            ProductCare4: "Iron on low temperature."
+        },
+        ProductPicture1:"/Item6.png",
+    },
+    {
+        ID: 7,
+        ProductName: "Imperial Alpaca Hoodie",
+        ProductType: "Jackets",
+        Price: 525.00,
+        ProductCategory: "Female",
+        ProductInformation: {
+            ProductDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ProductCare1: "Hand wash using cold water.",
+            ProductCare2: "Do not using bleach.",
+            ProductCare3: "Hang it to dry.",
+            ProductCare4: "Iron on low temperature."
+        },
+        ProductPicture1:"/Item7.png",
+    },
+    {
+        ID: 8,
+        ProductName: "Flex Push Button Bomber",
+        ProductType: "Jackets",
+        Price: 225.00,
+        ProductCategory: "Male",
+        ProductInformation: {
+            ProductDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ProductCare1: "Hand wash using cold water.",
+            ProductCare2: "Do not using bleach.",
+            ProductCare3: "Hang it to dry.",
+            ProductCare4: "Iron on low temperature."
+        },
+        ProductPicture1:"/Item8.png",
+    },
+    {
+        ID: 9,
+        ProductName: "Muscle Tank",
+        ProductType: "T Shirts",
+        Price: 75.00,
+        ProductCategory: "Female",
+        ProductInformation: {
+            ProductDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ProductCare1: "Hand wash using cold water.",
+            ProductCare2: "Do not using bleach.",
+            ProductCare3: "Hang it to dry.",
+            ProductCare4: "Iron on low temperature."
+        },
+        ProductPicture1:"/Item9.png",
+    },
+    {
+        ID: 3,
+        ProductName: "Brushed Bomber",
+        ProductType: "Jackets",
+        Price: 225.00,
+        ProductCategory: "Female",
+        ProductInformation: {
+            ProductDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ProductCare1: "Hand wash using cold water.",
+            ProductCare2: "Do not using bleach.",
+            ProductCare3: "Hang it to dry.",
+            ProductCare4: "Iron on low temperature."
+        },
+        ProductPicture1:"/Item10.png",
+    },
+    {
+        ID: 4,
+        ProductName: "Raglan Sweatshirt",
+        ProductType: "Sweater",
+        Price: 195.00,
+        ProductCategory: "Male",
+        ProductInformation: {
+            ProductDetails: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            ProductCare1: "Hand wash using cold water.",
+            ProductCare2: "Do not using bleach.",
+            ProductCare3: "Hang it to dry.",
+            ProductCare4: "Iron on low temperature."
+        },
+        ProductPicture1:"/Item11.png",
+    },
+];
+
+export default ItemsList;
--- src/views/Homepage/Thirdsection.tsx
+++ src/views/Homepage/Thirdsection.tsx
@@ -1,9 +1,15 @@
+import ItemsList from "@/items/itemslist"
+import Image from "next/image"
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 export default function Thirdsection() {
     return(
         <div>
             <div className="flex flex-col items-center justify-center space-y-5">
                 <span className="text-xs text-blue-600 font-bold">PRODUCTS</span>
                 <h2 className="scroll-m-20 pb-2 text-4xl font-bold tracking-tight first:mt-0">Check What We Have</h2>
+            </div>
+            <div>
             </div>
         </div>
     )
