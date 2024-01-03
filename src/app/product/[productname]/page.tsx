87736459c2c1dac413c71dc206fe4618a5b6a491
Muhammad Haris <mharis01470017@gmail.com>
2024-01-03 13:25:48 +0500

    hackathon-two

--- src/app/product/[productname]/page.tsx
+++ src/app/product/[productname]/page.tsx
@@ -1,4 +1,6 @@
+import React, {useState} from "react";
 import ProductsList from "@/products/Productslist";
+import Image from "next/image";
 
 export async function generateStaticParams() {
   return ProductsList.map((product) => ({
@@ -10,10 +12,21 @@ export default function Page({ params }: { params: { productname: string } }) {
   const Product = ProductsList.filter(
     (product) => product.ProductName.replace(/\s+/g, '-') === `${params.productname}`
   );
+  const [index, setIndex] = useState(0);
+
   return (
     <main>
       <div className="px-20">
-        {Product[0].ProductName}
+        <div className="flex">
+          <div className="flex flex-col">
+            {Product[0].ProductPicture.map((picture, ind) => (
+              <Image key={ind} src={picture} alt="" width={100} height={100} onMouseEnter={() => setIndex(ind)}/>              
+            ))}
+          </div>
+          <div className="w-80 h-full">
+              <Image src={Product[0].ProductPicture[0] && Product[0].ProductPicture[index]} alt="" className="w-full h-full"/>
+          </div>
+        </div>
       </div>
     </main>
   );
