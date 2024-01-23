c6b7036ee67ca5203df10c94b4d33fc2939d4647
Muhammad Haris <mharis01470017@gmail.com>
2024-01-23 23:34:40 +0500

    hackathon-two

--- src/views/Productpage.tsx
+++ src/views/Productpage.tsx
@@ -6,41 +6,50 @@ import { Plus, Minus, ShoppingCart } from 'lucide-react';
 
 export default function Productpage(Product:{ ID: number; ProductName: string; ProductType: string; Price: number; ProductCategory: string; ProductDetails: string; ProductCare: string[]; ProductPicture: string[];}[]) {
   const [index, setIndex] = useState(0);
+  const [value, setValue] = useState<number>(1)
+  const increment = () => {
+    setValue(value + 1);
+  };
+  const decrement = () => {
+    if (value > 1) {
+      setValue(value - 1);
+    }
+  };
 
   return (
-    <main>
-      <div className="flex px-20">
-        <div className="flex">
-          <div className="flex flex-col cursor-pointer">
+    <main className="bg-[#FCFCFC]">
+      <div className="flex px-20 justify-between pt-20">
+        <div className="flex flex-2 gap-8">
+          <div className="flex flex-col gap-4">
             {Product[0].ProductPicture.map((picture, ind) => (
-              <Image key={ind} src={picture} alt="" width={100} height={100} onMouseEnter={() => setIndex(ind)}/>              
+              <Image key={ind} src={picture} alt="" width={100} height={100} onMouseEnter={() => setIndex(ind)} className="cursor-pointer"/>              
             ))}
           </div>
-          <div>
-              <Image src={Product[0].ProductPicture[0] && Product[0].ProductPicture[index]} alt="" width={200} height={200}/>
+          <div className="w-4/5 h-full">
+              <Image src={Product[0].ProductPicture[0] && Product[0].ProductPicture[index]} alt="" width={590} height={1500}/>
           </div>
         </div>
         <div>
           <div>
-            <h3>{Product[0].ProductName}</h3>
-            <span>{Product[0].ProductType}</span>
+            <h3 className="font-normal text-3xl text-[#212121]">{Product[0].ProductName}</h3>
+            <span className="font-semibold text-xl opacity-30">{Product[0].ProductType}</span>
           </div>
           <div>
-            <p>SELECT SIZE</p>
-            <ul className="flex">
-              <li>XS</li>
-              <li>S</li>
-              <li>M</li>
-              <li>L</li>
-              <li>XL</li>
+            <p className="font-bold text-sm text-[#212121]">SELECT SIZE</p>
+            <ul className="flex gap-4">
+              <li className="flex font-bold w-2/5 h-2/5 justify-center items-center cursor-pointer rounded-full text-base text-[#666666] hover:shadow-md">XS</li>
+              <li className="flex font-bold w-2/5 h-2/5 justify-center items-center cursor-pointer rounded-full text-base text-[#666666] hover:shadow-md">S</li>
+              <li className="flex font-bold w-2/5 h-2/5 justify-center items-center cursor-pointer rounded-full text-base text-[#666666] hover:shadow-md">M</li>
+              <li className="flex font-bold w-2/5 h-2/5 justify-center items-center cursor-pointer rounded-full text-base text-[#666666] hover:shadow-md">L</li>
+              <li className="flex font-bold w-2/5 h-2/5 justify-center items-center cursor-pointer rounded-full text-base text-[#666666] hover:shadow-md">XL</li>
             </ul>
           </div>
           <div className="flex">
             <h4>Quantity:</h4>
             <div>
-              <Button><Minus/></Button>
-              <span>0</span>
-              <Button><Plus/></Button>
+              <Button onClick={decrement}><Minus/></Button>
+              <span>{value}</span>
+              <Button onClick={increment}><Plus/></Button>
             </div>
           </div>
           <div className="flex">
@@ -61,7 +70,7 @@ export default function Productpage(Product:{ ID: number; ProductName: string; P
           <h4>PRODUCT CARE</h4>
           <ul className="ml-6 list-disc">
             {Product[0].ProductCare.map((list, indexx) => (
-              <li key={indexx} >{list}</li>
+              <li key={indexx}>{list}</li>
             ))}
           </ul>
         </div>
