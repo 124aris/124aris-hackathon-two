8c42db9e862da1fe22cad7ebf68c86ab91e1305a
Muhammad Haris <mharis01470017@gmail.com>
2024-01-26 23:36:40 +0500

    hackathon-two

--- src/views/Homepage/Fifthsection.tsx
+++ src/views/Homepage/Fifthsection.tsx
@@ -2,10 +2,13 @@ import InputWithButton from "@/components/InputButton"
 
 export default function Fifthsection() {
     return (
-        <main className="flex justify-center items-center flex-col pt-20 pb-40">
-            <h1 className="text-4xl font-bold text-[#212121] mb-4">Subscribe Our Newsletter</h1>
-            <p className="font-light text-base text-[#212121] mb-8">Get the latest information and promo offers directly</p>
-            <InputWithButton/>
+        <main className="flex justify-center items-center flex-col pt-20 pb-40 relative">
+            <p className="font-extrabold text-9xl leading-9 text-[#f2f3f7] absolute z-0">Newsletter</p>
+            <div className="z-10">  
+                <h1 className="text-4xl font-bold text-[#212121] mb-4">Subscribe Our Newsletter</h1>
+                <p className="font-light text-base text-[#212121] mb-8">Get the latest information and promo offers directly</p>
+                <InputWithButton/>
+            </div>
         </main>
     )
 }
--- src/views/Homepage/Fourthsection.tsx
+++ src/views/Homepage/Fourthsection.tsx
@@ -4,36 +4,41 @@ import Link from "next/link"
 
 export default function Fourthsection() {
     return (
-        <main className="px-32 py-32">
-            <div className="px-2 flex justify-end items-center font-bold text-[#212121] text-5xl">
-                <h1 className="basis-1/2">Unique and Authentic Vintage Designer Jewellery</h1>
-            </div>
-            <div className="grid grid-cols-2 items center mt-10">
-                <div className="grid grid-cols-2 justify-around items-center">
-                    <div className="flex flex-col mr-20 space-y-4">
-                        <h3 className="font-semibold text-lg text-[#212121]">Using Good Quality Materials</h3>
-                        <p className="font-light text-base text-[#212121]">Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
-                    </div>
-                    <div className="flex flex-col mr-20 space-y-4">
-                        <h3 className="font-semibold text-lg text-[#212121]">100% Handmade Products</h3>
-                        <p className="font-light text-base text-[#212121]">Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
-                    </div>
-                    <div className="flex flex-col mr-20 space-y-4">
-                        <h3 className="font-semibold text-lg text-[#212121]">Modern Fashion Design</h3>
-                        <p className="font-light text-base text-[#212121]">Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
-                    </div>
-                    <div className="flex flex-col mr-20 space-y-4">
-                        <h3 className="font-semibold text-lg text-[#212121]">Discount for Bulk Orders</h3>
-                        <p className="font-light text-base text-[#212121]">Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
-                    </div>
+        <main className="mb-32 mt-16">
+            <div className=" bg-gradient-to-b from-white via-white to-[#FBFCFF]">
+                <div className="px-36 flex justify-end font-bold text-[#212121] text-5xl">
+                    <h1 className="basis-1/2">Unique and Authentic Vintage Designer Jewellery</h1>
                 </div>
-                <div className="flex">
-                    <Image src={'/feature.webp'} alt="" width={300} height={350}/>
-                    <div className="flex flex-col justify-center text-justify space-y-10 ml-10">
-                        <p className="font-light text-base text-[#212121]">This piece is ethically crafted in our small family-owned workshop in Peru with unmatched attention to detail and care. The Natural color is the actual natural color of the fiber, undyed and 100% traceable.</p>
-                        <Button className="flex font-semibold text-sm rounded-none">
-                            <Link href="/products" className="font-bold text-lg">See All Product</Link>
-                        </Button>
+            </div>
+            <div className="bg-[#FBFCFF] pb-16">
+                <div className="grid grid-cols-2 items center pt-10 px-32">
+                    <div className="grid grid-cols-2 justify-around items-center relative">
+                        <p className="font-extrabold text-8xl absolute text-[#212121] opacity-5 z-10">Different from others</p>
+                        <div className="flex flex-col mr-20 space-y-4 z-10">
+                            <h3 className="font-semibold text-lg text-[#212121]">Using Good Quality Materials</h3>
+                            <p className="font-light text-base text-[#212121]">Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
+                        </div>
+                        <div className="flex flex-col mr-20 space-y-4 z-10">
+                            <h3 className="font-semibold text-lg text-[#212121]">100% Handmade Products</h3>
+                            <p className="font-light text-base text-[#212121]">Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
+                        </div>
+                        <div className="flex flex-col mr-20 space-y-4 z-10">
+                            <h3 className="font-semibold text-lg text-[#212121]">Modern Fashion Design</h3>
+                            <p className="font-light text-base text-[#212121]">Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
+                        </div>
+                        <div className="flex flex-col mr-20 space-y-4 z-10">
+                            <h3 className="font-semibold text-lg text-[#212121]">Discount for Bulk Orders</h3>
+                            <p className="font-light text-base text-[#212121]">Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                    <div className="flex">
+                        <Image src={'/feature.webp'} alt="" width={300} height={350}/>
+                        <div className="flex flex-col justify-center text-justify space-y-10 ml-10">
+                            <p className="font-light text-base text-[#212121]">This piece is ethically crafted in our small family-owned workshop in Peru with unmatched attention to detail and care. The Natural color is the actual natural color of the fiber, undyed and 100% traceable.</p>
+                            <Button className="flex font-semibold text-sm rounded-none">
+                                <Link href="/products" className="font-bold text-lg">See All Product</Link>
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </div>
